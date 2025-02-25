e09120481f3929f7c0ec5c71a8a4b7cc1fff9d9d
GitHub git update Action <action@github.com>
2025-02-25 17:16:06 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_EECV.h
+++ generated/controllers/generated/rusefi_generated_EECV.h
@@ -1877,7 +1877,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.02.24.EECV.2228615616"
+#define TS_SIGNATURE "rusEFI main.2025.02.25.EECV.2228615616"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
