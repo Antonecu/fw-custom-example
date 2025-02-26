6b53ae49a188ec0d3ed63d089b819a9f6938dcbb
Anton <81507355+Antonecu@users.noreply.github.com>
2025-02-26 18:03:49 +0100

    Update board_configuration.cpp

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -12,45 +12,59 @@ Gpio getWarningLedPin() {
 	return Gpio::Unassigned;
 }
 
-//static void setSdCardSpi3() {
-//    engineConfiguration->isSdCardEnabled = true;
-//	engineConfiguration->sdCardSpiDevice = SPI_DEVICE_3;
-//	engineConfiguration->spi3mosiPin = Gpio::C12;
-//	engineConfiguration->spi3misoPin = Gpio::C11;
-//	engineConfiguration->spi3sckPin = Gpio::C10;
-//	engineConfiguration->is_enabled_spi_3 = true;
-//	engineConfiguration->sdCardCsPin = Gpio::D2;
-//}
+static void setInjectorPins() {
+	engineConfiguration->injectionPins[0] = Gpio::D13;
+	engineConfiguration->injectionPins[1] = Gpio::D12;
+	engineConfiguration->injectionPins[2] = Gpio::D11;
+	engineConfiguration->injectionPins[3] = Gpio::D10;
+}
+
+static void setIgnitionPins() {
+	engineConfiguration->ignitionPins[0] = Gpio::E8;
+	engineConfiguration->ignitionPins[1] = Gpio::E9;
+	engineConfiguration->ignitionPins[2] = Gpio::E10;
+	engineConfiguration->ignitionPins[3] = Gpio::E11;
+}
+
+static void setupVbatt() {
+	// 470 high side/ 910 low side
+	engineConfiguration->analogInputDividerCoefficient = 1.55f;
+	
+	// 6.34k high side/ 1k low side
+	engineConfiguration->vbattDividerCoeff = (6.34 + 1) / 1; 
+
+	// Battery sense on PC3
+	engineConfiguration->vbattAdcChannel = EFI_ADC_13;
+
+	engineConfiguration->adcVcc = 3.3f;
+}
+
+static void setSdCardSpi3() {
+    engineConfiguration->isSdCardEnabled = true;
+	engineConfiguration->sdCardSpiDevice = SPI_DEVICE_3;
+	engineConfiguration->spi3mosiPin = Gpio::C12;
+	engineConfiguration->spi3misoPin = Gpio::C11;
+	engineConfiguration->spi3sckPin = Gpio::C10;
+	engineConfiguration->is_enabled_spi_3 = true;
+	engineConfiguration->sdCardCsPin = Gpio::D2;
+}
 
 // board-specific configuration setup
 void setBoardDefaultConfiguration() {
+	setInjectorPins();
+	setIgnitionPins();
+	setupVbatt();
+	setSdCardSpi3();
 
-//	setSdCardSpi3();
-	
-//      engineConfiguration->injectionPins[0] = Gpio::F13;
-//      engineConfiguration->ignitionPins[0] = Gpio::E15;
+	engineConfiguration->clt.adcChannel = EFI_ADC_10;
+	engineConfiguration->iat.adcChannel = EFI_ADC_7;
+	engineConfiguration->clt.config.bias_resistor = 2490;
+	engineConfiguration->iat.config.bias_resistor = 2490;
 
-//      engineConfiguration->triggerInputPins[0] = Gpio::B1;
-//	engineConfiguration->triggerInputPins[1] = Gpio::Unassigned;
-
-//	engineConfiguration->map.sensor.hwChannel = EFI_ADC_3;
-//	engineConfiguration->clt.adcChannel = EFI_ADC_1;
-//	engineConfiguration->iat.adcChannel = EFI_ADC_2;
-
-// Battery sense on PA0
-	engineConfiguration->vbattAdcChannel = EFI_ADC_0;
+        engineConfiguration->triggerInputPins[0] = Gpio::E0;
+	engineConfiguration->triggerInputPins[1] = Gpio::Unassigned;
 
 //CAN 1 bus overwrites
 	engineConfiguration->canRxPin = Gpio::D0;
-	engineConfiguration->canTxPin = Gpio::D1;
-
-// same 470 and 910 on all analog inputs
-   	engineConfiguration->analogInputDividerCoefficient = 1.55f;
-	engineConfiguration->adcVcc = 3.3f;
-
-// 6.34k high side/ 1k low side
-    	engineConfiguration->vbattDividerCoeff = (6.34 + 1) / 1;
-
-	engineConfiguration->clt.config.bias_resistor = 2490;
-	engineConfiguration->iat.config.bias_resistor = 2490;	
+	engineConfiguration->canTxPin = Gpio::D1;	
 }
