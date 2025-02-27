98ecf167b7bb6ddfad04107eb689c5f37b36214d
GitHub git update Action <action@github.com>
2025-02-27 22:36:05 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_EECV.h
+++ generated/controllers/generated/signature_EECV.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3611919728
-#define TS_SIGNATURE "rusEFI main.2025.02.27.EECV.3611919728"
+#define SIGNATURE_HASH 130429408
+#define TS_SIGNATURE "rusEFI main.2025.02.27.EECV.130429408"
