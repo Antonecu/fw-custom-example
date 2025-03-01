d976b860e9a35fe747982172cdc6235f165ce218
Anton <81507355+Antonecu@users.noreply.github.com>
2025-03-01 23:19:24 +0100

    Create board.h

---
+++ board.h
@@ -0,0 +1,17 @@
+/**
+ * @file board.h
+ *
+ * @date Feb 06, 2021
+ * @author Andrey Gusakov, 2021
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+/*
+ * MCU type as defined in the ST header.
+ * this declaration for stm32_registry.h
+ */
+#define STM32F407xx
+
+#endif /* BOARD_H */
