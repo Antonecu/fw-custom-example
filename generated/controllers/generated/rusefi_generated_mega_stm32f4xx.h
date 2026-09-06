2e2839b449e0f49120e81f0f8655fe35509605f9
GitHub git update Action <action@github.com>
2026-09-06 01:26:50 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_mega_stm32f4xx.h
+++ generated/controllers/generated/rusefi_generated_mega_stm32f4xx.h
@@ -2292,7 +2292,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI main.2026.09.05.mega_stm32f4xx.4020709435"
+#define TS_SIGNATURE "rusEFI main.2026.09.06.mega_stm32f4xx.4020709435"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
